24befb51d727a4e6efa61ed2d5f84bdc298e149e
rafaelvicenzo <rafaellordes.lordes@gmail.com>
2024-08-01 16:29:45 -0300

    bell notification dark

--- darkmode.js
+++ darkmode.js
@@ -1,13 +1,22 @@
 'use strict'
 
 document.addEventListener('DOMContentLoaded', () => {
-    const btnDark = document.querySelector('.slider');
-  
-    btnDark.addEventListener('click', function () {
+  const btnDark = document.querySelector('.slider');
+
+  btnDark.addEventListener('click', function () {
       document.body.classList.toggle('dark-mode');
-  
-      document.querySelectorAll('aside, .logo, a, .search, .dateTLZ, .transactionsColumn, .salesHistory, .copyright').forEach(element => {
+
+      document.querySelectorAll('aside, .logo, a, .search, .dateTLZ, .transactionsColumn, .salesHistory, .copyright, .notificationImage').forEach(element => {
           element.classList.toggle('dark-mode');
       });
-    });
+
+      const notificationIcon = document.querySelector('.notificationImage img');
+      
+      if (document.body.classList.contains('dark-mode')) {
+          notificationIcon.src = 'assets/img/icons/iconsDarkMode/bellDark.png'; 
+          notificationIcon.style = 'position: relative; left: 2px;'
+      } else {
+          notificationIcon.src = 'assets/img/icons/bell.png'; 
+      }
   });
+});
